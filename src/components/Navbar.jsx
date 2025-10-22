69de8afa10f504517556f765198e8a095ed8b58a
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-10-22 01:32:36 +0100

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center"></div>
+      <span className="logo">MixMaster</span>
+      <div className="nav-links">
+        <NavLink to="/" className="nav-link">
+          Home
+        </NavLink>
+        <NavLink to="/about" className="nav-link">
+          About
+        </NavLink>
+        <NavLink to="/newsletter" className="nav-link">
+          Newsletter
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>navbar</nav>
+    <>
+      <Navbar />
       <Outlet />
-    </div>
+    </>
   );
 };
 
