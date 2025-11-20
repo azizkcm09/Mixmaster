e30f9c140b09698743b1e582bcdfff7244216eee
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-11-20 17:34:05 +0100

    CocktailCard

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
           loader: landingLoader,
         },
         {
-          path: "/coctail",
+          path: "/coctail/:id",
           element: <Coctail />,
         },
         {
--- src/components/CoctailCard.jsx
+++ src/components/CoctailCard.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
-const CoctailCard = () => {
-  return <h2>CocktailCard</h2>;
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailCard";
+const CoctailCard = ({ image, name, id, info, glass }) => {
+  return (
+    <Wrapper>
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn ">
+          Details
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default CoctailCard;
