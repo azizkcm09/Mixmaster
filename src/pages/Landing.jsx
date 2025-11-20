6997b9ca1c28c0adbea6dc3680e2c615909389f8
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-11-20 16:36:30 +0100

    fetch drinks

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,17 @@
+import axios from "axios";
+import { useLoaderData } from "react-router-dom";
+
+const coctailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 export const loader = async () => {
-  return "something";
+  const searchTerm = "margarita";
+  const response = await axios.get(`${coctailSearchUrl}${searchTerm}`);
+  return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return (
     <div>
       <h1>Landing page</h1>
