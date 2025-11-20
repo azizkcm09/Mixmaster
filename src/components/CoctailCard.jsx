4809f62cba20c7a1eafca2ba699742a0978e52b8
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-11-20 17:22:41 +0100

    CocktailList

---
+++ src/components/CoctailCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoctailCard = () => {
+  return <h2>CocktailCard</h2>;
+};
+
+export default CoctailCard;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,21 +1,22 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import CocktailList from "../components/CocktailList";
 
 const coctailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 export const loader = async () => {
-  const searchTerm = "margarita";
+  const searchTerm = "a";
   const response = await axios.get(`${coctailSearchUrl}${searchTerm}`);
   return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
   const { drinks, searchTerm } = useLoaderData();
-  console.log(drinks);
+
   return (
-    <div>
-      <h1>Landing page</h1>
-    </div>
+    <>
+      <CocktailList drinks={drinks} />
+    </>
   );
 };
 
