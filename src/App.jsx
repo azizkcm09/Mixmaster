24ff81bf54d35358d37afe0fe55e9fef9f7f7839
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-11-20 15:59:28 +0100

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     {
       path: "/",
       element: <HomeLayout />,
+      errorElement: <Error />,
       children: [
         {
           index: true,
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh!</h3>
+          <p>The page you are looking for does not exist</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>Something went wrong!</h3>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
