d145743c762d236e92d0584ce466c836d8a5e911
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-11-20 15:42:29 +0100

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Navbar";
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <Link to="/"> Home page</Link>
-    </div>
+    <Wrapper>
+      <h3> About us </h3>
+      <p>
+        Welcome to MixMaster, your ultimate destination for discovering and
+        enjoying the best music mixes from around the world. Our platform is
+        dedicated to bringing you a diverse range of high-quality mixes, curated
+        by talented DJs and music enthusiasts.
+      </p>
+    </Wrapper>
   );
 };
 
